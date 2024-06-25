bd3afc88d6059f3a4eed53d74553f6f0ee9d8741
aliaa99511 <aliaaattia7@gmail.com>
2024-06-26 01:19:55 +0300

    init product

--- react_route_project/src/Components/Products/Products.jsx
+++ react_route_project/src/Components/Products/Products.jsx
@@ -2,13 +2,12 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 const Products = () => {
-
-    const [products, setproducts] = useState([])
+    const [products, setProducts] = useState([])
 
     async function getAllProducts() {
         const response = await axios.get('https://ecommerce.routemisr.com/api/v1/products')
-        console.log('products:', response.data.data);
-        setproducts(response.data.data)
+        console.log('products:', response.data.data)
+        setProducts(response.data.data)
     }
 
     useEffect(() => {
@@ -16,18 +15,19 @@ const Products = () => {
     }, [])
 
     return (
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-            {products.map((product) => {
-                <div className="col">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+            {console.log("s", products)}
+            {products.map((product) => (
+                <div className="col" key={product.id}>
                     <div className="card h-100">
                         <img src={product.imageCover} className="card-img-top" alt={product.title} />
-                        <div classNames="card-body">
+                        <div className="card-body">
                             <h5 className="card-title">{product.title}</h5>
                             <p className="card-text">{product.description}</p>
                         </div>
                     </div>
                 </div>
-            })}
+            ))}
         </div>
     )
 }
