46e625af9f2169e37d76d84f9be8ef306f30c2b0
aliaa attia <aliaa.attia@uranium-corp.com>
2024-06-25 17:24:42 +0300

    add

--- react_route_project/src/Pages/Register/Register.jsx
+++ react_route_project/src/Pages/Register/Register.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from 'formik';
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
     const handleRegisterSubmit = async (values) => {
+        setLoading(true);
         try {
             const response = await axios.post('https://ecommerce.routemisr.com/api/v1/auth/signup', values);
             console.log('Response:', response.data);
+            if (response.data.message === "success") {
+                navigate("/login");
+            }
         } catch (error) {
-            console.error('Error:', error.response ? error.response.data : error.message);
+            console.error('Error:', error);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -41,7 +51,7 @@ const Register = () => {
 
         if (!values.phone) {
             errors.phone = "Required";
-        } else if (!/^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/.test(values.phone)) {
+        } else if (!/^01[0-9]{9}$/.test(values.phone)) {
             errors.phone = 'Invalid phone number format';
         }
 
@@ -124,136 +134,13 @@ const Register = () => {
                         {formik.touched.phone && formik.errors.phone ? formik.errors.phone : ''}
                     </div>
                 </div>
-                <button type="submit" className="btn btn-success">Register</button>
+                <button type="submit" disabled={loading || !formik.dirty} className="btn btn-success">
+                    {loading ? <span className="spinner-border spinner-border-sm mr-2" role="status" aria-hidden="true"></span> : null}{" "}
+                    Register
+                </button>
             </form>
         </div>
     );
 };
 
 export default Register;
-
-
-// import React from "react";
-// import { useFormik } from 'formik';
-
-// const Register = () => {
-//     const handleRegisterSubmit = (values) => {
-//         console.log('values', values);
-//     };
-
-//     const validate = (values) => {
-//         const errors = {};
-//         if (!values.name) {
-//             errors.name = "Required";
-//         } else if (values.name.length < 4) {
-//             errors.name = 'Must be 4 characters or more';
-//         }
-
-//         if (!values.email) {
-//             errors.email = "Required";
-//         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-//             errors.email = 'Invalid email address';
-//         }
-
-//         if (!values.password) {
-//             errors.password = "Required";
-//         } else if (values.password.length < 8) {
-//             errors.password = 'Must be 8 characters or more';
-//         }
-
-//         if (!values.rePassword) {
-//             errors.rePassword = "Required";
-//         } else if (values.rePassword !== values.password) {
-//             errors.rePassword = 'Passwords must match';
-//         }
-
-//         if (!values.phone) {
-//             errors.phone = "Required";
-//         } else if (!/^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/.test(values.phone)) {
-//             errors.phone = 'Invalid phone number format';
-//         }
-
-//         return errors;
-//     };
-
-//     const formik = useFormik({
-//         initialValues: {
-//             name: "",
-//             email: "",
-//             password: "",
-//             rePassword: "",
-//             phone: ""
-//         },
-//         validate,
-//         onSubmit: handleRegisterSubmit,
-//     });
-
-//     return (
-//         <div className="register mt-30">
-//             <h4 className="mb-25">Register Now</h4>
-//             <form onSubmit={formik.handleSubmit}>
-//                 <div className="mb-3">
-//                     <label htmlFor="name" className="form-label">Name:</label>
-//                     <input
-//                         type="text"
-//                         className={`form-control ${formik.touched.name && formik.errors.name ? 'is-invalid' : ''}`}
-//                         id="name"
-//                         value={formik.values.name}
-//                         onChange={formik.handleChange}
-//                         {...formik.getFieldProps('name')}
-//                     />
-//                     <div className="invalid-feedback">
-//                         {formik.touched.name && formik.errors.name ? formik.errors.name : ''}
-//                     </div>
-//                 </div>
-//                 <div className="mb-3">
-//                     <label htmlFor="email" className="form-label">Email:</label>
-//                     <input
-//                         type="email"
-//                         className="form-control"
-//                         id="email"
-//                         value={formik.values.email}
-//                         onChange={formik.handleChange}
-//                         {...formik.getFieldProps('email')}
-//                     />
-//                 </div>
-//                 <div className="mb-3">
-//                     <label htmlFor="password" className="form-label">Password:</label>
-//                     <input
-//                         type="password"
-//                         className="form-control"
-//                         id="password"
-//                         value={formik.values.password}
-//                         onChange={formik.handleChange}
-//                         {...formik.getFieldProps('password')}
-//                     />
-//                 </div>
-//                 <div className="mb-3">
-//                     <label htmlFor="rePassword" className="form-label">Confirm Password:</label>
-//                     <input
-//                         type="password"
-//                         className="form-control"
-//                         id="rePassword"
-//                         value={formik.values.rePassword}
-//                         onChange={formik.handleChange}
-//                         {...formik.getFieldProps('rePassword')}
-//                     />
-//                 </div>
-//                 <div className="mb-3">
-//                     <label htmlFor="phone" className="form-label">Phone:</label>
-//                     <input
-//                         type="tel"
-//                         className="form-control"
-//                         id="phone"
-//                         value={formik.values.phone}
-//                         onChange={formik.handleChange}
-//                         {...formik.getFieldProps('phone')}
-//                     />
-//                 </div>
-//                 <button type="submit" className="btn btn-success">Register</button>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default Register;
