c25bed8d27840ddb874f66234272162661ed2e24
aliaa99511 <aliaaattia7@gmail.com>
2024-06-26 01:24:25 +0300

    init product

--- react_route_project/src/Components/Products/Products.jsx
+++ react_route_project/src/Components/Products/Products.jsx
@@ -15,19 +15,20 @@ const Products = () => {
     }, [])
 
     return (
-        <div className="row row-cols-1 row-cols-md-5 g-4">
-            {console.log("s", products)}
-            {products.map((product) => (
-                <div className="col" key={product.id}>
-                    <div className="card h-100">
-                        <img src={product.imageCover} className="card-img-top" alt={product.title} />
-                        <div className="card-body">
-                            <h5 className="card-title">{product.title}</h5>
-                            <p className="card-text">{product.description}</p>
+        <div className='container mt-30'>
+            <div className="row row-cols-1 row-cols-md-5 g-4">
+                {products.map((product) => (
+                    <div className="col" key={product.id}>
+                        <div className="card h-100">
+                            <img src={product.imageCover} className="card-img-top" alt={product.title} />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.title}</h5>
+                                <p className="card-text">{product.description}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
