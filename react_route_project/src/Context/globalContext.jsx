27c956fb5227e433d546fad1baef93feffc36aac
aliaa attia <aliaa.attia@uranium-corp.com>
2024-06-26 17:12:53 +0300

    init context

--- react_route_project/src/Context/globalContext.jsx
+++ react_route_project/src/Context/globalContext.jsx
@@ -2,6 +2,27 @@ import React, { createContext } from "react";
 
 export const globalContext = createContext()
 const GlobalContextProvider = (props) => {
+
+    let headers = {
+        token: localStorage.getItem("token")
+    }
+
+
+    // function adProductToCart(id) {
+    //     return axios.post("https://ecommerce.routemisr.com/api/vl/cart",
+    //         {
+    //             productid: id,
+    //         }, {
+
+    //         headers,
+    //     }
+    //     )
+    //         .then((response) => response)
+    //         .catch((err) => err)
+    // }
+
+
+
     return <globalContext.Provider value={{}}>
         {props.childern}
     </globalContext.Provider>
